a1e382e64ae143d4bce9dc280e45f2f509ec2cdf
Shahzeb Ali <126076773+Shahzeb-Ali-Web-Developer@users.noreply.github.com>
2024-04-15 19:14:49 +0500

    Update bill memo.py

--- bill memo.py
+++ bill memo.py
@@ -3,7 +3,7 @@ class Bill:
         self.shop = "MOBILO - Mobile City"
         self.tagline = "We deal in New & Used Mobile Phones."
         self.cell = 92308888857
-        self.location = "Shop # 19, Ghalib Market, Lahore"
+        self.location = "Shop # 29, Ghalib Market, Lahore"
         self.type = "Cash Memo"
 
         self.id = id
